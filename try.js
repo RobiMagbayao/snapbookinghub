5851759e018f833185521f852e5ca9fce19e90a2
RobiMagbayao <robieannmagbayao@gmail.com>
2023-12-17 11:32:44 +0800

    add-on

--- try.js
+++ try.js
@@ -6,15 +6,51 @@ if (storedFormData) {
     storedFormData.forEach(orderDetails => {
         const newEntry = document.createElement("div");
 
-
-        //PART THAT USER/ADMIN CAN SEE
-        newEntry.innerHTML = `
+        // Start building the inner HTML
+        let innerHTML = `
         <strong>Booking Number:</strong> ${orderDetails.bookingNumber}<br>
         <strong>Event Date:</strong> ${orderDetails.date}<br>
         <strong>Event Time:</strong> ${orderDetails.time}<br>
         <strong>Event Location:</strong> ${orderDetails.eventAddress}, ${orderDetails.municipality}, ${orderDetails.province}<br>
-        <strong>Total Price: </strong> ₱${orderDetails.totalPrice}<br>
-        <hr>`;
+        <strong>Total Price: </strong> ₱${orderDetails.totalPrice}<br>`;
+
+        // Check if add-ons are present in orderDetails
+        if (orderDetails.userAddOnBirthdayProps) {
+            innerHTML += `<strong>Add-On</strong>: ${orderDetails.userAddOnBirthdayProps}<br>`;
+        }
+        if (orderDetails.userAddOnWeddingProps) {
+            innerHTML += `<strong>Add-On</strong>: ${orderDetails.userAddOnWeddingProps}<br>`;
+        }
+        if (orderDetails.userAddOnGraduationProps) {
+            innerHTML += `<strong>Add-On</strong>: ${orderDetails.userAddOnGraduationProps}<br>`;
+        }
+        if (orderDetails.userAddOnPhotoFilter) {
+            innerHTML += `<strong>Add-On</strong>: ${orderDetails.userAddOnPhotoFilter}<br>`;
+        }
+        if (orderDetails.userAddOnVideoMessage) {
+            innerHTML += `<strong>Add-On</strong>: ${orderDetails.userAddOnVideoMessage}<br>`;
+        }
+        if (orderDetails.userAddOnBoomerang) {
+            innerHTML += `<strong>Add-On</strong>: ${orderDetails.userAddOnBoomerang}<br>`;
+        }
+        if (orderDetails.userAddOnPhoneBooth) {
+            innerHTML += `<strong>Add-On</strong>: ${orderDetails.userAddOnPhoneBooth}<br>`;
+        }
+        if (orderDetails.userAddOnPhotoBook) {
+            innerHTML += `<strong>Add-On</strong>: ${orderDetails.userAddOnPhotoBook}<br>`;
+        }
+        if (orderDetails.userAddOnBackdrop) {
+            innerHTML += `<strong>Add-On</strong>: ${orderDetails.userAddOnBackdrop}<br>`;
+        }
+        if (orderDetails.userAddOnPrintPhoto) {
+            innerHTML += `<strong>Add-On</strong>: ${orderDetails.userAddOnPrintPhoto}<br>`;
+        }
+
+        // Add a horizontal line to separate add-ons from other details
+        innerHTML += `<hr>`;
+
+        // Set the innerHTML for the newEntry
+        newEntry.innerHTML = innerHTML;
 
         // Append the entry to the output div
         document.getElementById("outputOrder").appendChild(newEntry);
