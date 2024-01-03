d7ed8921f07ff363776d2b6e2c2ea9c69c24042c
ekiel311 <griswoldlegacy@yahoo.com>
2024-01-03 20:10:24 +0800

    autofill in contact us

--- form.js
+++ form.js
@@ -1,5 +1,4 @@
 function submitForm() {
-
     // Get form values
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
@@ -16,19 +15,34 @@ function submitForm() {
         email: email,
         phone: phone,
         message: message,
-        timestamp: formattedDateTime
+        timestamp: formattedDateTime,
     };
 
     // Retrieve existing form data from localStorage or initialize an empty array
-    const existingFormData = JSON.parse(localStorage.getItem('formData')) || [];
+    const existingFormData = JSON.parse(localStorage.getItem("formData")) || [];
 
     // Add the new form data
     existingFormData.push(formData);
 
     // Store updated form data in localStorage
-    localStorage.setItem('formData', JSON.stringify(existingFormData));
+    localStorage.setItem("formData", JSON.stringify(existingFormData));
 
     alert("Your message has been submitted. We will get back to you as soon as we can.");
     window.location.href = "index.html";
+}
 
+// ----------------------------- //
+// Autofill if logged in
+let loggedIn = sessionStorage.getItem("loggedIn");
+let currentUser = sessionStorage.getItem("currentUser");
+let userData = JSON.parse(localStorage.getItem(currentUser));
+
+if (loggedIn === "true") {
+    document.getElementById("name").value = userData.firstName + " " + userData.lastName;
+    document.getElementById("email").value = userData.email;
+    document.getElementById("phone").value = userData.phone;
+
+    document.getElementById("name").disabled = true;
+    document.getElementById("email").disabled = true;
+    document.getElementById("phone").disabled = true;
 }
